57320375acb274fdaa3657383cd0cfc9538d2d7a
KAUANPEREIRA <66709503+KAUANPEREIRA@users.noreply.github.com>
2021-06-28 14:55:03 -0300

    alterações
    Alterações formulário

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function salvar(){
+    var nome = document.getElementById('nome')
+
+    if(nome.value==0 ){
+        alert('Preencha os dados corretamentes ! ')
+    }else{
+        alert('Dados cadastrados com sucesso!')
+    }
+}
